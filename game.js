383d2ab9c1b5773c6b4540b9feab94d0d3a685ed
mrmollusc <rayscambo@gmail.com>
2026-03-24 18:34:32 +1100

    Update game.js
    red squares can move across the canvas, key support!

--- game.js
+++ game.js
@@ -1,33 +1,81 @@
 const canvas = document.getElementById('myCanvas');
 const a = canvas.getContext('2d');
-
 const audioElement = new Audio("decibat_yellow.ogg");
 let z=0;
 let i=0;
+let rectx=0;
+function beat(x, y, s){
+    this.x=x;
+    this.y=y;
+    this.s=s;
+    a.fillStyle='red';
+    a.fillRect(this.x,this.y,100,100)
+    a.clearRect(this.x,this.y,100,100)
+    
+}
+
+
+
+//code
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function move(){
+    a.fillStyle = 'white';
+    a.fillRect(500,100,100,100);  
+
+    if(rectx<500){
+        rectx+=1;
+        await delay(1);
+        a.fillStyle = 'red';
+        beat(100+rectx,200);
+        beat(500+rectx,200);
+        requestAnimationFrame(move);
+    }
+    else{
+        rectx=0;
+        requestAnimationFrame(move);
+    }
+}
+
 
 
 
 
 function play(){
-    addEventListener('click', (event) =>{
+    addEventListener('keydown', (e) =>{
+    if(e.keyCode===80){
         if(z==0){
-    audioElement.play();
-        z=1;
-        document.getElementById("click").innerHTML = "<b>pause</b>";
-        return;
+            audioElement.play();
+            z=1;
+            document.getElementById("click").innerHTML = "<b>pause</b>";
+            return;
         }
         if(z==1){
-    audioElement.pause();
-        z=0;
-        document.getElementById("click").innerHTML = "<b>play</b>";
-        return;
+            audioElement.pause();
+            z=0;
+            document.getElementById("click").innerHTML = "<b>play</b>";
+            return;
         }
+    }
+    if(e.keyCode===82){
+            audioElement.pause();
+            audioElement.currentTime = 0;
+            z=0;
+            document.getElementById("click").innerHTML = "<b>play</b>";  
+            alert('restart');
+            return;
+    }
+
         
+        
+       
     })  
 }
 
 
-
+//run
 play();
+requestAnimationFrame(move);
 
-
+//sprites
